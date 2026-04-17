5ebe31155541732c7c54140e20e2fea15d0b6186
mansib-uddin101 <drivemansib@gmail.com>
2026-04-17 23:46:12 +0600

    statspage issue fixed

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import FriendDetails from './components/FriendDetails.jsx'
 import ErrorPage from './components/ErrorPage.jsx'
 import FriendProvider from './context/FriendContext.jsx'
 import TimelinePage from './pages/TimelinePage.jsx'
-import StatsPage from './pages/Statspage.jsx';
+import Statspage from './pages/Statspage.jsx';
 
 const router = createBrowserRouter([
   {
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/stats",
-        element: <StatsPage/>
+        element: <Statspage/>
       }
     ],
     errorElement: <ErrorPage/>
